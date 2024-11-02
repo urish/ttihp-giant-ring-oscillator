8a69a70d5080bc2ad10b5abac11a52011743acb6
Uri Shaked <uri@urishaked.com>
2024-11-02 22:09:22 +0200

    Initial version - 1001 stages

--- src/project.v
+++ src/project.v
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2024 Your Name
+ * Copyright (c) 2024 Uri Shaked
  * SPDX-License-Identifier: Apache-2.0
  */
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_urish_giant_ringosc (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,12 +16,39 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  parameter CHAIN_LENGTH = 1001;
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire [CHAIN_LENGTH-1:0] inv_in;
+  wire [CHAIN_LENGTH-1:0] inv_out;
+
+  assign inv_in[0] = ui_in[0];
+  assign inv_in[CHAIN_LENGTH-1:1] = inv_out[CHAIN_LENGTH-2:0];
+
+  assign uo_out[0] = inv_out[0];
+  assign uo_out[1] = inv_out[2];
+  assign uo_out[2] = inv_out[4];
+  assign uo_out[3] = inv_out[6];
+  assign uo_out[4] = inv_out[10];
+  assign uo_out[5] = inv_out[20];
+  assign uo_out[6] = inv_out[50];
+  assign uo_out[7] = inv_out[100];
+
+  assign uio_oe = 8'b11111111;
+  assign uio_out[0] = inv_out[200];
+  assign uio_out[1] = inv_out[500];
+  assign uio_out[2] = inv_out[1000];
+  assign uio_out[7:3] = 5'b0;
+
+  genvar i;
+  generate
+    for (i = 0; i < CHAIN_LENGTH; i = i + 1) begin : inv
+      inverter inv (
+          .in (inv_in[i]),
+          .out(inv_out[i])
+      );
+    end
+  endgenerate
+
+  wire _unused = &{ena, clk, rst_n};
 
 endmodule
