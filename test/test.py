3b58c7f53ee4a3df6148d83c0bd165dd6a986cbb
Uri Shaked <uri@urishaked.com>
2024-11-03 09:06:30 +0200

    test: basic inverter testbench

--- test/test.py
+++ test/test.py
@@ -10,8 +10,8 @@ from cocotb.triggers import ClockCycles
 async def test_project(dut):
     dut._log.info("Start")
 
-    # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, units="us")
+    # Set the clock period to 1 us (1 MHz)
+    clock = Clock(dut.clk, 1, units="us")
     cocotb.start_soon(clock.start())
 
     # Reset
@@ -23,18 +23,24 @@ async def test_project(dut):
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
 
-    dut._log.info("Test project behavior")
+    dut._log.info("Test inverters")
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    # Test for input 0
+    dut.ui_in.value = 0
 
-    # Wait for one clock cycle to see the output values
+    # Wait for one clock cycle to see the result
     await ClockCycles(dut.clk, 1)
 
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    # We expect all the inverter outputs to be 1
+    assert dut.uo_out.value == 0xff
+    assert dut.uio_out.value & 0x07 == 0x07
 
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+    # Test for input 1
+    dut.ui_in.value = 1
+
+    # Wait for one clock cycle to see the result
+    await ClockCycles(dut.clk, 1)
+
+    # We expect all the inverter outputs to be 1
+    assert dut.uo_out.value == 0x00
+    assert dut.uio_out.value & 0x07 == 0x00
